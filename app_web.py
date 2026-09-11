52e12a0d834a1b5ed27da169436e658cafde3769
Electro9109 <hero100k@outlook.com>
2026-09-11 16:29:24 +0530

    Change app title to P.A.R.S.E

--- app_web.py
+++ app_web.py
@@ -33,7 +33,7 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="Metallurgical RAG + Predictor",
+    page_title="P.A.R.S.E",
     page_icon="🔬",
     layout="wide",
     initial_sidebar_state="expanded",
